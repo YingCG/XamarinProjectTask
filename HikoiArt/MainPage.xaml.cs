03489d0d54412f11784391216a65aad857f22643
YingCG <yingchiugoh@gmail.com>
2021-10-02 12:39:52 +1300

    file setup

---
+++ HikoiArt/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HikoiArt
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Create_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Console.WriteLine("Welcome to Hokoi Art");
+        }
+
+        public void Entry_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs args)
+        {
+            Console.WriteLine(args.NewTextValue);
+        }
+
+        public void Entry_Completed(System.Object sender, System.EventArgs args)
+        {
+            Console.WriteLine("Enter Pressed");
+            Label newTodo = new Label();
+            newTodo.Text = InputField.Text;
+            newTodo.FontSize = 20;
+            Todolist.Children.Add(newTodo);
+        }
+    }
+}

c2d0430b3722d2b30a7309b7e5ab6d826cf65ebc
YingCG <yingchiu@live.com>
2021-10-02 20:41:26 +1300

    to do list with view model

--- HikoiArt/MainPage.xaml.cs
+++ HikoiArt/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -15,23 +16,25 @@ namespace HikoiArt
             InitializeComponent();
         }
 
-        public void Create_Clicked(System.Object sender, System.EventArgs e)
-        {
-            Console.WriteLine("Welcome to Hokoi Art");
-        }
+        //public void Create_Clicked(System.Object sender, System.EventArgs e)
+        //{
+        //    Console.WriteLine("Welcome to Hokoi Art");
+        //}
 
-        public void Entry_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs args)
-        {
-            Console.WriteLine(args.NewTextValue);
-        }
+        //public void Entry_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs args)
+        //{
+        //    Console.WriteLine(args.NewTextValue);
+        //}
 
-        public void Entry_Completed(System.Object sender, System.EventArgs args)
-        {
-            Console.WriteLine("Enter Pressed");
-            Label newTodo = new Label();
-            newTodo.Text = InputField.Text;
-            newTodo.FontSize = 20;
-            Todolist.Children.Add(newTodo);
-        }
-    }
+        //public void Entry_Completed(System.Object sender, System.EventArgs args)
+        //{
+        //    Console.WriteLine("Enter Pressed");
+        //    Label newTodo = new Label();
+        //    newTodo.Text = InputField.Text;
+        //    newTodo.FontSize = 20;
+        //    Todolist.Children.Add(newTodo);
+        //    InputField.Text = "";
+       }
+
+       
 }
---
+++ HikoiArt/ViewModel/TodoListViewModel.cs
@@ -0,0 +1,38 @@
+﻿using HikoiArt.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace HikoiArt.ViewModel
+{
+    class TodoListViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            TodoItems.Add(new TodoItem("todo 1", false));
+            TodoItems.Add(new TodoItem("todo 2", false));
+            TodoItems.Add(new TodoItem("todo 3", false));
+        }
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        public string NewTodoInputValue { get; set; }
+        void AddTodoItem()
+        {
+           TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+        }
+
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+        void RemoveTodoItem(object o)
+        {
+            TodoItem todoItemBeingRemoved = o as TodoItem;
+            Console.WriteLine("todoItemBeingRemoved.TodoText");
+            TodoItems.Remove(todoItemBeingRemoved);
+        }
+    }
+}
